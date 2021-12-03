bfddfdab38b22d0965b047a1f40f2ba7e2531684
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-12-03 23:02:52 +0100

    do not play

--- src/paramidib.nim
+++ src/paramidib.nim
@@ -12,14 +12,13 @@ template saveMusic*(wavFile: string, score: untyped) =
   var sf = tsf_load_filename(paramidi_soundfonts.getSoundFontPath("generaluser.sf2"))
   const sampleRate = 44100
   tsf_set_output(sf, TSF_MONO, sampleRate, 0)
-  var res = render[cshort](compile(score), sf, sampleRate)
-  # create the wav file and play it
-  const
-    writeToDisk = true # if false, the wav file will only exist in memory
-    padding = 500f # add a half second so it doesn't cut off abruptly
+  var res = render[cshort](compile(scoreObject), sf, sampleRate)
+  # create the wav file (without playing it)
   # writeToDisk:
   common.writeFile(wavFile, res.data, res.data.len.uint32, sampleRate)
-  common.play(wavFile, int(res.seconds * 1000f + padding))
+  #const
+  #  padding = 500f # add a half second so it doesn't cut off abruptly
+  #common.play(wavFile, int(res.seconds * 1000f + padding))
 
 template nbAudio*(wavFile: string) =
   nbText: """<audio controls>
